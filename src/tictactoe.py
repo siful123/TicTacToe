362e5fa474e3fb6e484acc16361afa0201b91c80
meyou <sifulsid@buffalo.edu>
2019-03-23 21:36:33 -0400

    First commit into making TicTacToe game

---
+++ src/tictactoe.py
@@ -0,0 +1,22 @@
+'''l = [1, 2, 3, 4, 5, 6, 7]
+print(l[1])
+print(l[-1])
+print(l[1:])
+print(l[1:2])
+'''
+
+x = [[0, 0, 0],
+     [0, 0, 0],
+     [0, 0, 0]]
+
+
+def tictactoeboard(player, row, column, display):
+    print("   a  b  c")
+
+    if not display:
+        x[row][column] = player
+    for count,row in enumerate(x):
+        print(count,row)
+
+
+tictactoeboard(1,0,1,False)
