a2a58dd58f710943b2a46004f66f33fed82b456a
meyou <sifulsid@buffalo.edu>
2019-03-23 22:00:01 -0400

    Understand Mutability and what the global keyword does

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -8,6 +8,87 @@ print(l[1:])
 print(l[1:2])
 '''
 
+#can change value of object but not object itself
+'''x = [1,2,3]
+print(id(x))
+
+def tictactoeboard():
+    #x = "A game"
+    x[1]=95
+    print(id(x))
+    print(x)
+tictactoeboard()
+print(x)
+print(id(x))
+'''
+
+#global word allows us to change the stuff inside the game variable
+'''game = "I want to play a game"
+print(id(game))
+
+def game_board():
+    global game
+    game = "A game"
+    print(id(game))
+    print(game)
+
+game_board()
+print(game)
+print(id(game))
+'''
+
+''' Mini Quiz to understand Mutability
+x = 1
+def test():
+    x = 2
+test()
+print(x)#1
+
+
+x = 1
+def test():
+    global x
+    x = 2
+test()
+print(x)#2
+
+
+x = [1]
+def test():
+    x = [2]
+test()
+print(x)#[1]
+
+
+x = [1]
+def test():
+    global x
+    x = [2]
+test()
+print(x)#[2]
+
+
+x = [1]
+def test():
+    x[0] = 2
+test()
+print(x)#[2]
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 x = [[0, 0, 0],
      [0, 0, 0],
      [0, 0, 0]]
@@ -22,4 +103,5 @@ def tictactoeboard(player, row, column, display):
         print(count,row)
 
 
-tictactoeboard(1,0,1,False)
+tictactoeboard(1,0,1,False) 
+'''
