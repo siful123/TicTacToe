5034cfcc82f1ebff5a4e18305efe1676ccea1482
meyou <sifulsid@buffalo.edu>
2019-04-02 02:00:47 -0400

    Make a game function requiring a winner.

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -73,34 +73,44 @@ test()
 print(x)#[2]
 '''
 import itertools
-x = [[1, 2, 1],
-     [0, 1, 2],
-     [0, 2, 1]]
 
-
-
-def tictactoeboard(game_map, player, row, column, display=False):
+def tictactoeboard(game_map, player=1, row=0, column=0, display=False):
     try:
+        if game_map[row][column] != 0:
+            print("This position is currently occupied!")
+            return game_map, False
         print("   0  1  2")
 
         if not display:
             game_map[row][column] = player
         for count,row in enumerate(game_map):
             print(count,row)
-        return game_map
+        return game_map, True
     except IndexError as e:
         print("Error: Make sure you input row/column as 0,1, or 2", e)
+        return game_map, False
 
     except Exception as e:
         print("Something went very wrong!", e)
+        return game_map, False
+
 
 def win (current_game):
+
+
+    def all_same(l):
+        if l.count(l[0]) == len(l) and l[0] != 0:
+            return True
+        else:
+            return False
+
 
     #Horizontal winner
     for row in x:
         print(row)
-        if row.count(row[0]) == len(row) and row[0] !=0:
+        if all_same(row):
             print(f"Player {row[0]} has gotten three in a row!")
+            return True
 
 
     #Vertical Winner
@@ -110,9 +120,9 @@ def win (current_game):
         for row in x:
             check.append(row[col])
             #print(check)
-        if check.count(check[0]) == len(check) and check[0] != 0:
+        if all_same(check):
             print(f"Player {check[0]} has gotten three down!")
-
+            return True
 
 
     #Diagonal winner
@@ -122,14 +132,24 @@ def win (current_game):
 
     for indexOfX in range(len(x)):
         diagonals.append(x[indexOfX][indexOfX])
-    if diagonals.count(diagonals[0]) == len(diagonals) and diagonals[0] != 0:
+    if all_same(diagonals):
         print(f"Player {diagonals[0]} has gotten three diagonally!")
+        return True
 
     diags = []
     for col,row in zip(cols,rows):
         diags.append(x[row][col])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+    if all_same(diags):
         print(f"Player {diags[0]} has gotten three diagonally!")
+        return True
+
+    return False
+
+
+
+
+
+
 
 play = True
 players = itertools.cycle([1, 2])
@@ -139,16 +159,28 @@ while play:
          [0, 0, 0]]
 
     game_won = False
+    x,_ = tictactoeboard(x, display=True)
     while not game_won:
         current_player = next(players)
         print(f"Players Turn: {current_player}")
-        column_choice = int(input("Play in column (0, 1, 2): "))
-        row_choice = int(input("Play in row (0, 1, 2): "))
-        game = tictactoeboard(x, current_player, row_choice, column_choice)
+        played = False
 
+        while not played:
+            column_choice = int(input("Play in column (0, 1, 2): "))
+            row_choice = int(input("Play in row (0, 1, 2): "))
+            x, played = tictactoeboard(x, current_player, row_choice, column_choice)
 
-
-
+        if win(x):
+            game_won = True
+            again = input("Do you want to play again? (yes or no)")
+            if again.upper() == "YES":
+                print("restarting")
+            if again.upper() == "NO":
+                print("deuces mooses, hope to see you again!")
+                play = False
+            else:
+                print("Not valid, goodbye and hope to see you again!")
+                play = False
 
 
 
